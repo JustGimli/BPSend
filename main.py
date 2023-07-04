d24109bc6cbcb036d9de18bb0e0831c1e60771ef
JustGimli <aleksnermer@gmail.com>
2023-07-04 09:38:48 +0300

    add files to send

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from aiogram import Bot, Dispatcher
 
 bot = Bot(token=os.getenv(
-    "TOKEN", "5311611984:AAE49xLBcV5aa9AMdvnm46vna6VFpM3N92Q"))
+    "TOKEN"))
 dp = Dispatcher(bot=bot)
 
 
@@ -16,8 +16,14 @@ async def send_message(id, message):
 
 
 async def main():
-    user_id = os.getenv("USER_ID", "1239134441")
-    message = os.getenv("MESSAGE", "Asd")
+    user_id = os.getenv("USER_ID")
+    message = os.getenv("MESSAGE")
+    files = os.getenv('FILES')
+
+    if files:
+        for file in files:
+            await bot.send_document(user_id, file)
+
     await send_message(user_id, message)
     await bot.close()
 
